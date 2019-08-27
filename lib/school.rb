96303818ea37db9d8370111a57fb441729bc75fa
Evan Jeffries <ejeffries14@gmail.com>
2019-08-27 17:38:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
+class School
+  
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+    roster[grade] << name
+  else
+    
+  end
+  
+  def grade
+    
+  end
+end
 # code here!
